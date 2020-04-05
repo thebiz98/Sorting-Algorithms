59bb9e2aa5eb94ecfcfb64add5c4df5347814bdc
thebiz98 <bishnutiwari963@gmail.com>
2020-04-05 18:41:12 +0530

    merge sort modified

--- mergeSort.c
+++ mergeSort.c
@@ -24,77 +24,76 @@ void swap(lf *a, lf *b)
 
 lint randomize(lint n)
 {
-	return (rand()%n+1);
+	return (rand()%n);
 }
 
 void shuffle(lf *a, lint n)
 {
-	lint i,index;
+	lint i,x;
 	for(i=0;i<n;i++)
 	{
-		index=randomize(n);
-		swap(&a[i],&a[index]);
+		x=randomize(n);
+		swap(&a[i],&a[x]);
 	}
 }
 
 void merge(lf *arr, lint l, lint m, lint r)
 {
-	lint a=m-l+1;
-	lint b=r-m,i=l,j=0,k=0;
-	lf le[a];
-	lf re[b];
-	while(j<a)
+	lint x=m-l+1;
+	lint y=r-m;
+	lint i=0,j=0,k=0;
+	lf *L=(lf *)malloc(x*sizeof(lf));
+	lf *R=(lf *)malloc(y*sizeof(lf));
+	for(i=0;i<x;i++)
 	{
-		le[j]=arr[i];
-		j++;
-		i++;
+		L[i]=arr[l+i];
 	}
-	while(k<b)
+	for(i=0;i<y;i++)
 	{
-		re[k]=arr[i];
-		k++;
-		i++;
+		R[i]=arr[m+i+1];
 	}
+	i=0;
 	j=0;
-	k=0;
-	i=l;
-	while(j<a&&k<b)
+	for(k=l;k<=r;k++)
 	{
-		count++;
-		move++;
-		if(le[j]<=re[k])
+		if((i<x)&&(j<y))
 		{
-			arr[i]=le[j];
+			count++;
+			move++;
+			if(L[i]<=R[j])
+			{
+				arr[k]=L[i];
+				i++;
+			}
+			else
+			{
+				arr[k]=R[j];
+				j++;
+			}
+		}
+		else if(i<x&&j>=y)
+		{
+			move++;
+			arr[k]=L[i];
+			i++;
+		}
+		else if(i>=x&&j<y)
+		{
+			move++;
+			arr[k]=R[j];
 			j++;
 		}
-		else
-		{
-			arr[i]=re[k];
-			k++;
-		}
-		i++;
 	}
-	while(j<a)
-	{
-		move++;
-		arr[i]=le[j];
-		i++;
-		j++;
-	}
-	while(k<b)
-	{
-		move++;
-		arr[i]=re[k];
-		i++;
-		k++;
-	}
+	free(L);
+	free(R);
 }
 
 void mergeSort(lf *arr, lint l, lint r)
 {
+	lint m;
 	if(l<r)
 	{
-		lint m=l+(r-l)/2;
+		m=(l+r)/2;
 		mergeSort(arr,l,m);
 		mergeSort(arr,m+1,r);
 		merge(arr,l,m,r);
@@ -105,58 +104,67 @@ int main(int argc, char *argv[])
 {
 	FILE *f=fopen(argv[argc-1],"r");
 	FILE *res=fopen("results.csv","a");
+	lint n,i,x=1;
+	lint n2,n3,tmp;
+	lf n1,c1,c2,c3,c4;
 	char dist=argv[argc-1][0];
-	lf *arr;
-	lint n,i,shuff=1;
-	lf n1,n2,n3,c1,c2,c3,c4;
-	lint len=strlen(argv[argc-1])+5+5;
-	char out_file[len+1];
+	int len=strlen(argv[argc-1])+11;
+	char out_file[len];
 	out_file[0]='\0';
 	strcat(out_file,argv[argc-1]);
 	strcat(out_file,"merge");
 	strcat(out_file,"check");
-	srand(time(0));
+	out_file[len-1]='0';
+	lf *a;
 	fscanf(f,"%lld",&n);
-	arr=(lf *)malloc(n*sizeof(lf));
+	a=(lf *)malloc(n*sizeof(lf));
 	for(i=0;i<n;i++)
 	{
-		fscanf(f,"%lf",arr+i);
+		fscanf(f,"%lf",a+i);
 	}
 	fclose(f);
-	for(;shuff<=5;shuff++)
+	srand(time(0));
+	for(;x<=5;x++)
 	{
 		count=0;
 		move=0;
-		printf("Shuffle no.: %lld\n",shuff);
+		printf("\nShuffle no.: %lld\n",x);
+		shuffle(a,n);
+		out_file[len-1]++;
+		f=fopen(out_file,"w");
+		printf("\nThe output is being written to the file: %s\n",out_file);
 		start=clock();
-		mergeSort(arr, 0, n-1);
+		mergeSort(a,0,n-1);
 		end=clock();
 		cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
-		out_file[len]=(char)(shuff+'0');
-		f=fopen(out_file,"w");
-		printf("The output is written to file: %s\n\n",out_file);
 		fprintf(f,"%lld ",n);
 		for(i=0;i<n;i++)
 		{
-			fprintf(f,"%lf ",arr[i]);
+			if(dist=='u')
+			{
+				tmp=(lint)a[i];
+				fprintf(f,"%lld ",tmp);
+			}
+			else
+			{
+				fprintf(f,"%lf ",a[i]);
+			}
 		}
-		printf("\n\nThe number of comparisons is: %lld\n",count);
-		printf("The number of moves is: %lld\n",move);
-		printf("The time taken is: %lf\n\n",cpu_time_used);
+		fclose(f);
 		n1=n*(log(n)/log(2));
 		n2=n*n;
 		n3=10*n;
-		c1=count/n;
+		c1=count/(n*1.0);
 		c2=count/n1;
-		c3=count/n2;
-		c4=count/n3;
-		fprintf(res,"MergeSort,%c,%lld,%lld,%lf,%lf,%lf,%lld,%lf,%lf,%lf,%lf,%lld,%lf\n",dist,shuff,n,n1,n2,n3,count,c1,c2,c3,c4,move,cpu_time_used);
-		fclose(f);
-		shuffle(arr,n);
+		c3=count/(n2*1.0);
+		c4=count/(n3*1.0);
+		printf("The number of comparisons is: %lld\n",count);
+		printf("The number of moves is: %lld\n",move);
+		printf("The time taken for the sorting is: %lf\n",cpu_time_used);
+		fprintf(res,"Merge_Sort,%c,%lld,%lld,%lf,%lld,%lld,%lld,%lf,%lf,%lf,%lf,%lld,%lf\n",dist,x,n,n1,n2,n3,count,c1,c2,c3,c4,move,cpu_time_used);
 	}
 	fprintf(res,"\n");
 	fclose(res);
-	free(arr);
+	free(a);
 	return 0;
 }
-
