e610200503c039ebdab1685cde51343087cb0f57
thebiz98 <bishnutiwari963@gmail.com>
2020-05-18 00:18:07 +0530

    refining the code and increasing readability

--- mergeSort.c
+++ mergeSort.c
@@ -15,19 +15,19 @@ lint count=0,move=0;
 clock_t start,end;
 double cpu_time_used;
 
-void swap(lf *a, lf *b)
+void swap(lf *a, lf *b)							//function to swap values
 {
 	lf tmp=*a;
 	*a=*b;
 	*b=tmp;
 }
 
-lint randomize(lint n)
+lint randomize(lint n) 							//function to generate random index for shuffling
 {
 	return (rand()%n);
 }
 
-void shuffle(lf *a, lint n)
+void shuffle(lf *a, lint n) 						//function to shuffle data
 {
 	lint i,x;
 	for(i=0;i<n;i++)
@@ -37,7 +37,7 @@ void shuffle(lf *a, lint n)
 	}
 }
 
-void merge(lf *arr, lint l, lint m, lint r)
+void merge(lf *arr, lint l, lint m, lint r)				//merge function to merge the subarrays; used by the merge sort procedure
 {
 	lint x=m-l+1;
 	lint y=r-m;
@@ -45,21 +45,17 @@ void merge(lf *arr, lint l, lint m, lint r)
 	lf *L=(lf *)malloc(x*sizeof(lf));
 	lf *R=(lf *)malloc(y*sizeof(lf));
 	for(i=0;i<x;i++)
-	{
 		L[i]=arr[l+i];
-	}
 	for(i=0;i<y;i++)
-	{
 		R[i]=arr[m+i+1];
-	}
 	i=0;
 	j=0;
 	for(k=l;k<=r;k++)
 	{
 		if((i<x)&&(j<y))
 		{
-			count++;
-			move++;
+			count++;					// counts the number of comparisons performed. this data will be used to analyse complexity of the procedure
+			move++;						// counts the number of times an element is moved from its position
 			if(L[i]<=R[j])
 			{
 				arr[k]=L[i];
@@ -73,13 +69,13 @@ void merge(lf *arr, lint l, lint m, lint r)
 		}
 		else if(i<x&&j>=y)
 		{
-			move++;
+			move++;						//move counter increased again as elements moved from original location
 			arr[k]=L[i];
 			i++;
 		}
 		else if(i>=x&&j<y)
 		{
-			move++;
+			move++;						//move counter increased again as element moved from original location
 			arr[k]=R[j];
 			j++;
 		}
@@ -88,7 +84,7 @@ void merge(lf *arr, lint l, lint m, lint r)
 	free(R);
 }
 
-void mergeSort(lf *arr, lint l, lint r)
+void mergeSort(lf *arr, lint l, lint r)					//the procedure to implement the merge sort algorithm
 {
 	lint m;
 	if(l<r)
@@ -102,40 +98,37 @@ void mergeSort(lf *arr, lint l, lint r)
 
 int main(int argc, char *argv[])
 {
-	FILE *f=fopen(argv[argc-1],"r");
-	FILE *res=fopen("results.csv","a");
-	lint n,i,x=1;
-	lint n2,n3,tmp;
-	lf n1,c1,c2,c3,c4;
+	FILE *f=fopen(argv[argc-1],"r");							//opening file that contains the dataset
+	FILE *res=fopen("results.csv","a");							//opening file that will store the result analysis
+	lint n,i,x=1,tmp;
+	lf n1,c1;
 	char dist=argv[argc-1][0];
 	int len=strlen(argv[argc-1])+11;
 	char out_file[len];
 	out_file[0]='\0';
 	strcat(out_file,argv[argc-1]);
-	strcat(out_file,"mergecheck0");
+	strcat(out_file,"mergecheck0");								//generating filename that will store sorted data for verification
 	lf *a;
 	fscanf(f,"%lld",&n);
 	a=(lf *)malloc(n*sizeof(lf));
 	for(i=0;i<n;i++)
-	{
 		fscanf(f,"%lf",a+i);
-	}
-	fclose(f);
+	fclose(f);										//dataset read into the program and dataset file closed
 	srand(time(0));
-	for(;x<=5;x++)
+	for(;x<=5;x++)										//loop to control shuffling of data
 	{
 		count=0;
-		move=0;
+		move=0;										//move and comparison counter initialised
 		printf("\nShuffle no.: %lld\n",x);
 		shuffle(a,n);
 		out_file[len-1]++;
-		f=fopen(out_file,"w");
+		f=fopen(out_file,"w");								//opening file to store sorted dataset
 		printf("\nThe output is being written to the file: %s\n",out_file);
 		start=clock();
-		mergeSort(a,0,n-1);
+		mergeSort(a,0,n-1);								//calling the mergesort procedure with the dataset
 		end=clock();
 		cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
-		fprintf(f,"%lld ",n);
+		fprintf(f,"%lld ",n);								//writing sorted data to output file
 		for(i=0;i<n;i++)
 		{
 			if(dist=='u')
@@ -148,21 +141,16 @@ int main(int argc, char *argv[])
 				fprintf(f,"%lf ",a[i]);
 			}
 		}
-		fclose(f);
-		n1=n*(log(n)/log(2));
-		n2=n*n;
-		n3=10*n;
-		c1=count/(n*1.0);
-		c2=count/n1;
-		c3=count/(n2*1.0);
-		c4=count/(n3*1.0);
+		fclose(f);									//output data file closed
+		n1=n*(log(n)/log(2));								//as the algorithm is supposed to have O(n lg n) complexity, we will use this data for our analysis
+		c1=count/n1;									//data to be used for analysis
 		printf("The number of comparisons is: %lld\n",count);
 		printf("The number of moves is: %lld\n",move);
 		printf("The time taken for the sorting is: %lf\n",cpu_time_used);
-		fprintf(res,"Merge_Sort,%c,%lld,%lld,%lf,%lld,%lld,%lld,%lf,%lf,%lf,%lf,%lld,%lf\n",dist,x,n,n1,n2,n3,count,c1,c2,c3,c4,move,cpu_time_used);
+		fprintf(res,"Merge_Sort,%c,%lld,%lld,%lf,%lld,%lf,%lld,%lf\n",dist,x,n,n1,count,c1,move,cpu_time_used);			//data generated and written to results.csv for analysis of the algorithm
 	}
 	fprintf(res,"\n");
-	fclose(res);
-	free(a);
+	fclose(res);										//results file closed
+	free(a);										//memory deallocated
 	return 0;
 }
--- quickSort.c
+++ quickSort.c
@@ -15,19 +15,19 @@ lint count=0,swaps=0;
 clock_t start,end;
 double cpu_time_used;
 
-void swap(lf *a, lf *b)
+void swap(lf *a, lf *b)								//function to swap values
 {
 	lf tmp=*a;
 	*a=*b;
 	*b=tmp;
 }
 
-lint randomize(lint n)
+lint randomize(lint n)								//function to generate random index for shufflling data
 {
 	return (rand()%n);
 }
 
-void shuffle(lf *a, lint n)
+void shuffle(lf *a, lint n)							//function to shuffle data
 {
 	lint i,x;
 	for(i=0;i<n;i++)
@@ -37,27 +37,27 @@ void shuffle(lf *a, lint n)
 	}
 }
 
-lint partition(lf *arr, lint l, lint h)
+lint partition(lf *arr, lint l, lint h)						//function to partition dataset, to be used by the quick sort procedure
 {
-	lf pivot=arr[h];
+	lf pivot=arr[h];							//last element of the array chosen as pivot
 	lint i=l-1;
 	lint j;
 	for(j=l;j<h;j++)
 	{
-		count++;
+		count++;							//counting number of comparisons to use for analysis later
 		if(arr[j]<pivot)
 		{
-			swaps++;
+			swaps++;						//counting the number of times data values are swapped
 			i++;
 			swap(&arr[i],&arr[j]);
 		}
 	}
 	swaps++;
 	swap(&arr[i+1],&arr[h]);
-	return i+1;
+	return i+1;								//returning position of pivot
 }
 
-void quickSort(lf *arr, lint low, lint high)
+void quickSort(lf *arr, lint low, lint high)					//function to implement quick sort algorithm
 {
 	if(low<high)
 	{
@@ -69,37 +69,34 @@ void quickSort(lf *arr, lint low, lint high)
 
 int main(int argc, char *argv[])
 {
-	FILE *f=fopen(argv[argc-1],"r");
-	FILE *res=fopen("results.csv","a");
-	lint n,i,x=1;
-	lint n2,n3,tmp;
+	FILE *f=fopen(argv[argc-1],"r");			//opening the file containing the dataset
+	FILE *res=fopen("results.csv","a");			//opening the file that will store the results for analysis
+	lint n,i,x=1,n2,n3,tmp;
 	lf n1,c1,c2,c3,c4;
 	char dist=argv[argc-1][0];
 	int len=strlen(argv[argc-1])+11;
 	char out_file[len];
 	out_file[0]='\0';
 	strcat(out_file,argv[argc-1]);
-	strcat(out_file,"quickcheck0");
+	strcat(out_file,"quickcheck0");				//generating filename of the file that will store the sorted data for verification
 	lf *a;
 	fscanf(f,"%lld",&n);
 	a=(lf *)malloc(n*sizeof(lf));
 	for(i=0;i<n;i++)
-	{
 		fscanf(f,"%lf",a+i);
-	}
-	fclose(f);
+	fclose(f);						//dataset read into program and dataset file closed
 	srand(time(0));
-	for(;x<=5;x++)
+	for(;x<=5;x++)						//loop to control shuffling of data
 	{
 		count=0;
-		swaps=0;
+		swaps=0;					//move and swap count initialised
 		printf("\nShuffle no.: %lld\n",x);
 		shuffle(a,n);
 		out_file[len-1]++;
-		f=fopen(out_file,"w");
+		f=fopen(out_file,"w");				//file to store sorted dataset opened
 		printf("The output is being written to the file: %s\n",out_file);
 		start=clock();
-		quickSort(a,0,n-1);
+		quickSort(a,0,n-1);				//quicksort procedure called
 		end=clock();
 		cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
 		fprintf(f,"%lld ",n);
@@ -115,18 +112,15 @@ int main(int argc, char *argv[])
 				fprintf(f,"%lf ",a[i]);
 			}
 		}
-		fclose(f);
+		fclose(f);					//sorted data written to file and file closed
 		n1=n*(log(n)/log(2));
 		n2=n*n;
-		n3=10*n;
-		c1=count/(n*1.0);
-		c2=count/n1;
-		c3=count/(n2*1.0);
-		c4=count/(n3*1.0);
+		c1=count/n1;
+		c2=count/(n2*1.0);				//calculating data for analysis. both comparisons by n lg n and n*n are stored as the complexity of this method varies from  O(n lg n) to O(n^2)
 		printf("The number of comparisons is: %lld\n",count);
 		printf("The number of swaps is: %lld\n",swaps);
 		printf("The time taken for the sorting is: %lf\n",cpu_time_used);
-		fprintf(res,"Quick_Sort,%c,%lld,%lld,%lf,%lld,%lld,%lld,%lf,%lf,%lf,%lf,%lld,%lf\n",dist,x,n,n1,n2,n3,count,c1,c2,c3,c4,swaps,cpu_time_used);
+		fprintf(res,"Quick_Sort,%c,%lld,%lld,%lf,%lld,%lld,%lf,%lf,%lld,%lf\n",dist,x,n,n1,n2,count,c1,c2,swaps,cpu_time_used);
 	}
 	fprintf(res,"\n");
 	fclose(res);
